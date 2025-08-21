214f8a6a45c9881f25706c2ee916a781053ee03f
Aniket <aniketkashidak007@gmail.com>
2025-08-21 23:56:05 +0530

    change in spark file

--- Spark.java
+++ Spark.java
@@ -2,4 +2,9 @@ class Demo {
     public static void main(String[] args) {
         System.out.println("Hello");
     }
+
+    public void add()
+    {
+        System.out.println("Values Added");
+    }
 }
